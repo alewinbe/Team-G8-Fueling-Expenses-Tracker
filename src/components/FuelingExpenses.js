cab0083e9b70a829be8efb9d6d4fe08471178828
Alexander Winberg <alewinbe@gmail.com>
2022-05-24 16:01:44 +0300

    added total money spent, fuel and distance driven to list

--- src/components/FuelingExpenses.js
+++ src/components/FuelingExpenses.js
@@ -4,9 +4,19 @@ import { GlobalContext } from "../context/GlobalState";
 export const FuelingExpenses= () => {
   const { refuelings } = useContext(GlobalContext);
 
-  const fuelAmounts = refuelings.map((refueling) => refueling.fuelAmount);
+  const moneySpent = refuelings.map((refueling) => refueling.moneySpent);
+  const fuelAmount = refuelings.map((refueling) => refueling.fuelAmount);
+  const distanceDriven = refuelings.map((refueling) => refueling.distanceDriven);
 
-  const income = fuelAmounts
+  const totalMoneySpent = moneySpent
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const totalFuelAmount = fuelAmount
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const totalDistanceDriven = distanceDriven
     .filter((item) => item > 0)
     .reduce((acc, item) => (acc += item), 0)
     .toFixed(2);
@@ -15,19 +25,19 @@ export const FuelingExpenses= () => {
     <div className="inc-exp-container">
       <div>
         <h4>Money Spent €</h4>
-        <p className="money plus">{income}</p>
+        <p className="money plus">{totalMoneySpent}</p>
 
         <h4>Total consumption (L)</h4>
-        <p className="money plus">{income}</p>
+        <p className="money plus">{totalFuelAmount}</p>
 
         <h4>Total Distance (km)</h4>
-        <p className="money plus">{income}</p>
+        <p className="money plus">{totalDistanceDriven}</p>
 
         <h4>Avg. Expenses/100km (€)</h4>
-        <p className="money plus">{income}</p>
+        <p className="money plus">{}</p>
 
         <h4>Avg. Consumption/100km (L)</h4>
-        <p className="money plus">{income}</p>
+        <p className="money plus">{}</p>
       </div>
     </div>
   );
